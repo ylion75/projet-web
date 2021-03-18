f2a382aba3350be231fa074702ffd2ae95e9163d
Yan Lion <ylion75@gmail.com>
2021-03-18 22:34:14 +0100

    Suppression de la cascade de if dans signup

--- signup.php
+++ signup.php
@@ -19,32 +19,39 @@ if($existinguser[0] > 0){
     goto display;
 }
 
-if(isset($_FILES['avatar']) AND !empty($_FILES['avatar']['name'])) {
-    $maxSize = 2097152;
-    $validExtensions = array('jpg', 'jpeg', 'gif', 'png');
-    if($_FILES['avatar']['size'] <= $maxSize) {
-        $extensionUpload = strtolower(substr(strrchr($_FILES['avatar']['name'], '.'), 1));
-        if(in_array($extensionUpload, $validExtensions)) {
-            $path = "members/avatars/".$_POST["login"].".".$extensionUpload;
-            $result = move_uploaded_file($_FILES['avatar']['tmp_name'], $path);
-             if($result) {
-                $updateavatar = $db->prepare('UPDATE user SET avatar = :avatar WHERE id = :id');
-                $updateavatar->execute(array(
-                    'avatar' => $_POST["login"].".".$extensionUpload,
-                    'id' => $_POST["login"]
-                ));
-                header('Location: profil.php?id='.$_POST["login"]);
-            } else {
-                $msg = "Erreur durant l'importation de votre photo de profil";
-            }
-
-        } else {
-            $msg = "Votre photo de profil doit être au format jpg, jpeg, gif ou png";
-        }
-    } else {
-        $msg = "Votre photo de profil ne doit pas dépasser 2Mo";
-    }
+if(!isset($_FILES['avatar']) || !isset($_FILES['avatar']['name'])){
+    goto display;
 }
+
+$maxSize = 2097152;
+$validExtensions = array('jpg', 'jpeg', 'gif', 'png');
+
+if($_FILES['avatar']['size'] > $maxSize){
+    $error="Votre photo de profil ne doit pas dépasser 2Mo";
+    goto display;
+}
+
+$extensionUpload = strtolower(substr(strrchr($_FILES['avatar']['name'], '.'), 1));
+
+if(!in_array($extensionUpload, $validExtensions)){
+    $error="Votre photo de profil doit être au format jpg, jpeg, gif ou png";
+    goto display;
+}
+
+$path = "members/avatars/".$_POST["login"].".".$extensionUpload;
+$result = move_uploaded_file($_FILES['avatar']['tmp_name'], $path);
+
+if(!$result){
+    $error="Erreur durant l'importation de votre photo de profil";
+    goto display;
+}
+
+$updateavatar = $db->prepare('UPDATE user SET avatar = :avatar WHERE id = :id');
+$updateavatar->execute(array(
+    'avatar' => $_POST["login"].".".$extensionUpload,
+    'id' => $_POST["login"]
+));
+
 
 $password = password_hash($_POST["password"], PASSWORD_BCRYPT);
 $request = $db->prepare("INSERT INTO user (login, password,email) VALUES (?,?,?)");
