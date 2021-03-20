0b0ffd79d20f0e875a862ae53c4d79dc4ae5ecea
ya-naka <y.naitkaci@gmail.com>
2021-03-20 20:46:01 +0100

    modif structure index.php

--- index.php
+++ index.php
@@ -2,18 +2,29 @@
 session_start();
 require("config.php");
 require("db_connect.php");
-require("header.php");
 
 function uri($path){
     global $config;
     return $config["uri_prefix"].$path;
 }
 
-$path = explode("?", basename($_SERVER['REQUEST_URI']));
-
-if(!isset($config["routes"][$path[0]])){
-    include($config["routes"][""].".php");
-}else{
-    include($config["routes"][$path[0]].".php");
+function render($view){
+    ob_start();
+    require("views/$view.php");
+    $content = ob_get_contents();
+    ob_end_clean();
+    require("views/layout.php");
 }
-require("footer.php");
+
+$requestURI = $_SERVER["REQUEST_URI"];
+$requestURI = substr($requestURI, strlen($config["uri_prefix"]));
+$handler = $config["routes"][$requestURI];
+
+//$path = explode("?", basename($_SERVER['REQUEST_URI']));
+
+//if(!isset($config["routes"][$path[0]])){
+if(!isset($handler)){
+    require("handlers/".$config["routes"][""].".php");
+}else{
+    require("handlers/".$config["routes"][$path[0]].".php");
+}
